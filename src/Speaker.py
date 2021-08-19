2ab50281e3e11918f5a2b074d5e6bb58b9b44752
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-08-19 23:33:32 +0200

    Make arduino buzzer sound instead of computer

--- src/Speaker.py
+++ src/Speaker.py
@@ -1,11 +1,10 @@
-import winsound
+import time
 from threading import Thread
-
+from ArduinoBoard import *
 
 class Speaker:
 
-    sound_frequency = 600  # Hertz
-    dot_sound_duration = 100  # Milliseconds
+    dot_sound_duration = 0.2  # Seconds
     dash_sound_duration = dot_sound_duration * 3
 
     @classmethod
@@ -21,8 +20,15 @@ class Speaker:
 
     @classmethod
     def play_dot_sound(cls):
-        winsound.Beep(cls.sound_frequency, cls.dot_sound_duration)
+        cls._play_sound(cls.dot_sound_duration)
 
     @classmethod
     def play_dash_sound(cls):
-        winsound.Beep(cls.sound_frequency, cls.dash_sound_duration)
+        cls._play_sound(cls.dash_sound_duration)
+
+    @classmethod
+    def _play_sound(cls, duration):
+        t_end = time.time() + duration
+        while time.time() < t_end:
+            ArduinoBoard.buzzer.write(True)
+        ArduinoBoard.buzzer.write(False)
